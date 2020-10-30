2484d068c4714d61793aec1c7e0a903852e58a62
rivesone <rivesone@outlook.com>
2020-10-30 17:16:29 +0000

    startup project
    prima versione funzionante - manca solo il filtro sui codici/descrizioni

---
+++ Print_barcode/Form1.Designer.cs
@@ -0,0 +1,452 @@
+﻿namespace Print_barcode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+			this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+			this.printDialog1 = new System.Windows.Forms.PrintDialog();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+			this.edtDes = new System.Windows.Forms.TextBox();
+			this.edtCod = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.lvMain = new System.Windows.Forms.ListView();
+			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.numPos = new System.Windows.Forms.NumericUpDown();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.PNL = new System.Windows.Forms.Panel();
+			this.lblDES2 = new System.Windows.Forms.Label();
+			this.BCODELBL = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.BCODE = new System.Windows.Forms.PictureBox();
+			this.lblDES1 = new System.Windows.Forms.Label();
+			this.lblART = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.lblDES3 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+			this.splitContainer2.Panel1.SuspendLayout();
+			this.splitContainer2.Panel2.SuspendLayout();
+			this.splitContainer2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numPos)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.PNL.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.BCODE)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// printDocument1
+			// 
+			this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+			// 
+			// printPreviewDialog1
+			// 
+			this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+			this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+			this.printPreviewDialog1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+			this.printPreviewDialog1.Document = this.printDocument1;
+			this.printPreviewDialog1.Enabled = true;
+			this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+			this.printPreviewDialog1.Name = "printPreviewDialog1";
+			this.printPreviewDialog1.Visible = false;
+			// 
+			// printDialog1
+			// 
+			this.printDialog1.Document = this.printDocument1;
+			this.printDialog1.UseEXDialog = true;
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.statusStrip1.Location = new System.Drawing.Point(0, 480);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+			this.statusStrip1.Size = new System.Drawing.Size(1169, 22);
+			this.statusStrip1.TabIndex = 15;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// splitContainer2
+			// 
+			this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer2.Name = "splitContainer2";
+			this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer2.Panel1
+			// 
+			this.splitContainer2.Panel1.Controls.Add(this.edtDes);
+			this.splitContainer2.Panel1.Controls.Add(this.edtCod);
+			this.splitContainer2.Panel1.Controls.Add(this.label3);
+			this.splitContainer2.Panel1.Controls.Add(this.label1);
+			// 
+			// splitContainer2.Panel2
+			// 
+			this.splitContainer2.Panel2.Controls.Add(this.splitContainer1);
+			this.splitContainer2.Size = new System.Drawing.Size(1169, 480);
+			this.splitContainer2.SplitterDistance = 42;
+			this.splitContainer2.TabIndex = 17;
+			// 
+			// edtDes
+			// 
+			this.edtDes.Location = new System.Drawing.Point(383, 15);
+			this.edtDes.Name = "edtDes";
+			this.edtDes.Size = new System.Drawing.Size(195, 20);
+			this.edtDes.TabIndex = 1;
+			// 
+			// edtCod
+			// 
+			this.edtCod.Location = new System.Drawing.Point(135, 15);
+			this.edtCod.Name = "edtCod";
+			this.edtCod.Size = new System.Drawing.Size(122, 20);
+			this.edtCod.TabIndex = 1;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(290, 18);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(87, 13);
+			this.label3.TabIndex = 0;
+			this.label3.Text = "Filtro Descrizione";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(64, 19);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(65, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Filtro Codice";
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.lvMain);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.numPos);
+			this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+			this.splitContainer1.Panel2.Controls.Add(this.button3);
+			this.splitContainer1.Panel2.Controls.Add(this.button2);
+			this.splitContainer1.Panel2.Controls.Add(this.PNL);
+			this.splitContainer1.Panel2.Controls.Add(this.button1);
+			this.splitContainer1.Size = new System.Drawing.Size(1169, 434);
+			this.splitContainer1.SplitterDistance = 765;
+			this.splitContainer1.SplitterWidth = 3;
+			this.splitContainer1.TabIndex = 17;
+			// 
+			// lvMain
+			// 
+			this.lvMain.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader6,
+            this.columnHeader5});
+			this.lvMain.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.lvMain.FullRowSelect = true;
+			this.lvMain.HideSelection = false;
+			this.lvMain.Location = new System.Drawing.Point(0, 0);
+			this.lvMain.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.lvMain.Name = "lvMain";
+			this.lvMain.Size = new System.Drawing.Size(765, 434);
+			this.lvMain.Sorting = System.Windows.Forms.SortOrder.Ascending;
+			this.lvMain.TabIndex = 10;
+			this.lvMain.UseCompatibleStateImageBehavior = false;
+			this.lvMain.View = System.Windows.Forms.View.Details;
+			this.lvMain.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.lvMain_ItemSelectionChanged);
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "Codice";
+			this.columnHeader1.Width = 100;
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Text = "Articolo";
+			this.columnHeader2.Width = 90;
+			// 
+			// columnHeader3
+			// 
+			this.columnHeader3.Text = "Descrizione";
+			this.columnHeader3.Width = 160;
+			// 
+			// columnHeader4
+			// 
+			this.columnHeader4.Text = "Descrizione2";
+			this.columnHeader4.Width = 150;
+			// 
+			// columnHeader6
+			// 
+			this.columnHeader6.Text = "Descrizione 3";
+			this.columnHeader6.Width = 150;
+			// 
+			// columnHeader5
+			// 
+			this.columnHeader5.Text = "Barcode";
+			this.columnHeader5.Width = 100;
+			// 
+			// numPos
+			// 
+			this.numPos.Location = new System.Drawing.Point(170, 294);
+			this.numPos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.numPos.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+			this.numPos.Name = "numPos";
+			this.numPos.Size = new System.Drawing.Size(80, 20);
+			this.numPos.TabIndex = 18;
+			this.numPos.Value = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+			this.numPos.ValueChanged += new System.EventHandler(this.numPos_ValueChanged);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(20, 286);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(105, 54);
+			this.pictureBox1.TabIndex = 17;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(281, 347);
+			this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(70, 48);
+			this.button3.TabIndex = 16;
+			this.button3.Text = "Print";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(20, 347);
+			this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(94, 48);
+			this.button2.TabIndex = 15;
+			this.button2.Text = "Preview";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Visible = false;
+			// 
+			// PNL
+			// 
+			this.PNL.BackColor = System.Drawing.Color.White;
+			this.PNL.Controls.Add(this.lblDES3);
+			this.PNL.Controls.Add(this.lblDES2);
+			this.PNL.Controls.Add(this.BCODELBL);
+			this.PNL.Controls.Add(this.label2);
+			this.PNL.Controls.Add(this.BCODE);
+			this.PNL.Controls.Add(this.lblDES1);
+			this.PNL.Controls.Add(this.lblART);
+			this.PNL.Location = new System.Drawing.Point(20, 22);
+			this.PNL.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.PNL.Name = "PNL";
+			this.PNL.Size = new System.Drawing.Size(346, 250);
+			this.PNL.TabIndex = 14;
+			// 
+			// lblDES2
+			// 
+			this.lblDES2.AccessibleDescription = "";
+			this.lblDES2.AutoSize = true;
+			this.lblDES2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblDES2.Location = new System.Drawing.Point(27, 181);
+			this.lblDES2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.lblDES2.Name = "lblDES2";
+			this.lblDES2.Size = new System.Drawing.Size(56, 22);
+			this.lblDES2.TabIndex = 8;
+			this.lblDES2.Text = "Text2";
+			// 
+			// BCODELBL
+			// 
+			this.BCODELBL.AutoSize = true;
+			this.BCODELBL.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.BCODELBL.Location = new System.Drawing.Point(142, 102);
+			this.BCODELBL.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.BCODELBL.Name = "BCODELBL";
+			this.BCODELBL.Size = new System.Drawing.Size(117, 27);
+			this.BCODELBL.TabIndex = 7;
+			this.BCODELBL.Text = "bcode_lbl";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(26, 130);
+			this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(62, 27);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "ART:";
+			// 
+			// BCODE
+			// 
+			this.BCODE.Location = new System.Drawing.Point(130, 2);
+			this.BCODE.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.BCODE.Name = "BCODE";
+			this.BCODE.Size = new System.Drawing.Size(201, 95);
+			this.BCODE.TabIndex = 5;
+			this.BCODE.TabStop = false;
+			// 
+			// lblDES1
+			// 
+			this.lblDES1.AutoSize = true;
+			this.lblDES1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblDES1.Location = new System.Drawing.Point(27, 159);
+			this.lblDES1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.lblDES1.Name = "lblDES1";
+			this.lblDES1.Size = new System.Drawing.Size(56, 22);
+			this.lblDES1.TabIndex = 4;
+			this.lblDES1.Text = "Text1";
+			// 
+			// lblART
+			// 
+			this.lblART.AutoSize = true;
+			this.lblART.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblART.Location = new System.Drawing.Point(84, 130);
+			this.lblART.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.lblART.Name = "lblART";
+			this.lblART.Size = new System.Drawing.Size(67, 27);
+			this.lblART.TabIndex = 3;
+			this.lblART.Text = "Text0";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(150, 368);
+			this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(100, 27);
+			this.button1.TabIndex = 13;
+			this.button1.Text = "Reload XLS";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// lblDES3
+			// 
+			this.lblDES3.AccessibleDescription = "";
+			this.lblDES3.AutoSize = true;
+			this.lblDES3.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblDES3.Location = new System.Drawing.Point(27, 203);
+			this.lblDES3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.lblDES3.Name = "lblDES3";
+			this.lblDES3.Size = new System.Drawing.Size(56, 22);
+			this.lblDES3.TabIndex = 9;
+			this.lblDES3.Text = "Text2";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1169, 502);
+			this.Controls.Add(this.splitContainer2);
+			this.Controls.Add(this.statusStrip1);
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Name = "Form1";
+			this.Text = "MAS Label Printer";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.splitContainer2.Panel1.ResumeLayout(false);
+			this.splitContainer2.Panel1.PerformLayout();
+			this.splitContainer2.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+			this.splitContainer2.ResumeLayout(false);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.numPos)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.PNL.ResumeLayout(false);
+			this.PNL.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.BCODE)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+    private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.SplitContainer splitContainer2;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.ListView lvMain;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+		private System.Windows.Forms.ColumnHeader columnHeader2;
+		private System.Windows.Forms.ColumnHeader columnHeader3;
+		private System.Windows.Forms.ColumnHeader columnHeader4;
+		private System.Windows.Forms.ColumnHeader columnHeader5;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Panel PNL;
+		private System.Windows.Forms.Label lblDES2;
+		private System.Windows.Forms.Label BCODELBL;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.PictureBox BCODE;
+		private System.Windows.Forms.Label lblDES1;
+		private System.Windows.Forms.Label lblART;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox edtDes;
+		private System.Windows.Forms.TextBox edtCod;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ColumnHeader columnHeader6;
+		private System.Windows.Forms.NumericUpDown numPos;
+		private System.Windows.Forms.Label lblDES3;
+	}
+}
+
---
+++ Print_barcode/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Printing;
+
+using System.IO; //legge file
+using NPOI.HSSF.Model; // InternalWorkbook
+using NPOI.HSSF.UserModel; // HSSFWorkbook, HSSFSheet
+using System.Text.RegularExpressions;
+
+namespace Print_barcode
+{
+
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+    InitializeComponent();
+    }
+    //lettura file xls
+    HSSFWorkbook wb;
+    HSSFSheet sh;
+    //generazione immagine barcode
+    Bitmap bmp;
+
+    
+    void _genBarcode(string _pText)
+    {
+      if (_pText!="")
+      {
+        try
+        {
+          Zen.Barcode.CodeEan13BarcodeDraw barcode = Zen.Barcode.BarcodeDrawFactory.CodeEan13WithChecksum;
+          BCODE.Image = barcode.Draw(_pText, BCODE.Height, 2);
+					BCODELBL.Text = _pText + IsValidGtin(_pText);
+
+          //_pText;
+        }
+        catch (System.ArgumentException)
+        {
+          MessageBox.Show("Codice a Barre Errato");
+          //throw;
+        }
+
+      } 
+    }
+
+    public string IsValidGtin(string code)
+    {
+      if (code != (new Regex("[^0-9]")).Replace(code, ""))
+      {
+        // is not numeric
+        return "X";
+      }
+      // pad with zeros to lengthen to 14 digits
+      /* switch (code.Length)
+      {
+        case 8:
+          code = "000000" + code;
+          break;
+        case 12:
+          code = "00" + code;
+          break;
+        case 13:
+          code = "0" + code;
+          break;
+        case 14:
+          break;
+        default:
+          // wrong number of digits
+          return "X";
+      }
+      */
+      // calculate check digit
+      int[] a = new int[13];
+      a[0] = int.Parse(code[0].ToString());
+      a[1] = int.Parse(code[1].ToString()) * 3;
+      a[2] = int.Parse(code[2].ToString());
+      a[3] = int.Parse(code[3].ToString()) * 3;
+      a[4] = int.Parse(code[4].ToString());
+      a[5] = int.Parse(code[5].ToString()) * 3;
+      a[6] = int.Parse(code[6].ToString());
+      a[7] = int.Parse(code[7].ToString()) * 3;
+      a[8] = int.Parse(code[8].ToString());
+      a[9] = int.Parse(code[9].ToString()) * 3;
+      a[10] = int.Parse(code[10].ToString());
+      a[11] = int.Parse(code[11].ToString()) * 3;
+      //a[12] = int.Parse(code[12].ToString()) * 3;
+      int sum = a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] + a[8] + a[9] + a[10] + a[11];
+        //+ a[12];
+      int check = (10 - (sum % 10)) % 10;
+      return check.ToString();
+
+      //// evaluate check digit
+      //int last = int.Parse(code[13].ToString());
+      //return check == last;
+    }
+
+    public bool _selectPrinter()
+    {
+      bool _result = false;
+      if (printDialog1.ShowDialog() == DialogResult.OK)
+      {
+        printDocument1.PrinterSettings.PrinterName = printDialog1.PrinterSettings.PrinterName;
+        printDocument1.PrinterSettings.Copies = printDialog1.PrinterSettings.Copies;
+        _result = true;
+      }
+
+      return _result;
+    }
+
+    
+    void _readXLS ()
+    {
+      if (File.Exists("CodiciABarre.xls"))
+      {
+        //ripulisco la itemlistbox
+        lvMain.Items.Clear();
+        //inizio lettura file excel
+        using (var fs = new FileStream("CodiciABarre.xls", FileMode.Open, FileAccess.Read))
+        {
+          wb = new HSSFWorkbook(fs);
+          sh = (HSSFSheet)wb.GetSheet("Etichette");
+
+          for (int row = 0; row <= sh.LastRowNum; row++) //Loop the records upto filled row  
+          {
+            if (sh.GetRow(row) != null) //null is when the row only contains empty cells   
+            {
+              //carico la listview
+              ListViewItem _lvItem;
+              _lvItem = lvMain.Items.Add(sh.GetRow(row).GetCell(0).ToString());
+              for (int I = 1; I<=5; I++)
+              {
+                if (sh.GetRow(row).GetCell(I) == null)
+                  _lvItem.SubItems.Add("");
+                else
+              _lvItem.SubItems.Add(sh.GetRow(row).GetCell(I).ToString());
+              }
+              //string value = sh.GetRow(row).GetCell(0).ToString(); //Here for sample , I just save the value in "value" field, Here you can write your custom logics...  
+              //MessageBox.Show(value);
+            }
+          }
+        }
+        lvMain.Items[0].Selected = true;
+      }
+      else
+        MessageBox.Show("File etichette non trovato");
+    }
+    
+    private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+    {
+      bmp = new Bitmap(PNL.Width, PNL.Height);
+      PNL.DrawToBitmap(bmp, new Rectangle(0, 0, PNL.Width, PNL.Height));
+      e.Graphics.DrawImage(bmp, 0, 0);
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      //carico list view
+      _readXLS();
+      numPos.Value = BCODE.Left;
+            
+    }
+    
+    private void button1_Click_1(object sender, EventArgs e)
+    {
+      _readXLS();
+    }
+
+    private void button2_Click_1(object sender, EventArgs e)
+    {
+      printPreviewDialog1.ShowDialog();
+    }
+
+    private void button3_Click_1(object sender, EventArgs e)
+    {
+      if (_selectPrinter())
+        printDocument1.Print();
+    }
+
+    private void lvMain_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+    {
+      //al cambio dell'indice di selezione aggiorno i controlli dell'etichetta
+      if (e.IsSelected)
+      //if (lvMain.SelectedIndices.Count > 0)
+      {
+       // if (lvMain.SelectedItems[0].SubItems[1].Text != "")
+          lblART.Text = lvMain.SelectedItems[0].SubItems[1].Text;
+
+       // if (lvMain.SelectedItems[0].SubItems[2].Text != "")
+          lblDES1.Text = lvMain.SelectedItems[0].SubItems[2].Text;
+
+       // if (lvMain.SelectedItems[0].SubItems[3].Text != "")
+          lblDES2.Text = lvMain.SelectedItems[0].SubItems[3].Text;
+
+        lblDES3.Text = lvMain.SelectedItems[0].SubItems[4].Text;
+
+        if (lvMain.SelectedItems[0].SubItems[5].Text != "")
+          _genBarcode(lvMain.SelectedItems[0].SubItems[5].Text);
+      }
+    }
+
+		private void numPos_ValueChanged(object sender, EventArgs e)
+		{
+      //sposto il riquadro del docie a barre
+      BCODE.Left = Convert.ToInt32(numPos.Value);
+
+    }
+	}
+}
